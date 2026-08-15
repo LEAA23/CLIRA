f31ceeaee3b90164440803bbec789ab6c3d63322
LEAA23 <luisernesto2969@gmail.com>
2026-08-14 18:35:10 -0600

    Controlador de grupos terminado

--- backend/src/controllers/GroupsController.ts
+++ backend/src/controllers/GroupsController.ts
@@ -4,15 +4,16 @@ import { Group } from "../models/Group";
 export class GroupsControlller {
     static createGroup = async (req: Request, res: Response) => {
         //Extraemos el nombre del grupo que escribio el usuario
-        const {name} = req.body;
+        const {name, bgImage} = req.body;
         try {
             //Creamos y guardamos el grupo con los datos en la base de datos
             const group = await Group.create({
                 name,
+                bgImage,
                 teacher: req.user.id
             });
 
-            return res.status(200).send("Grupo Creado Correctamente");
+            return res.status(200).send("Grupo creado correctamente");
 
         } catch (error) {
             console.log(error)
@@ -28,6 +29,38 @@ export class GroupsControlller {
             //Econtramos todos los grupos que pertencen al maetsro mediante el id del mismo
             const groups = await Group.findAll( { where: { teacher} } );
             return res.status(200).json({ groups });
+        } catch (error) {
+            return res.status(500).json( { error: "Error interno del servidor" } );
+        }
+    }
+
+    static updateGroup = async( req: Request, res: Response ) => {
+        const { name, bgImage } = req.body;
+
+        try {
+            const groupExists = req.group;
+
+            //El grupo existe y ademas lo esta modificando el usuario con permisos
+            groupExists.name = name;
+            groupExists.bgImage = bgImage;
+
+            //Guardamos los cambios en la BD
+            await groupExists.save();
+            return res.status(200).send("El grupo se modifico correctamente");
+
+        } catch (error) {
+            return res.status(500).json( { error: "Error interno del servidor" } );
+        }
+    }
+
+    static deleteGroup = async( req: Request, res: Response ) => {
+        try {
+            const groupExists = req.group;
+
+            //El usuario tiene permisos y el grupo si existe
+            await groupExists.destroy();
+            return res.status(200).send("Grupo eliminado correctamente");
+
         } catch (error) {
             return res.status(500).json( { error: "Error interno del servidor" } );
         }
