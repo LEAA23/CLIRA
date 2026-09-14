20a0c67b906219a69b5c4dd12c74bf9233f267dc
LEAA23 <luisernesto2969@gmail.com>
2026-09-14 15:34:05 -0600

    feat: Se creo el modelo de Comment que crea la tabla de Comments

---
+++ backend/src/models/Comment.ts
@@ -0,0 +1,40 @@
+import { CreationOptional, DataTypes, InferAttributes, InferCreationAttributes, Model } from "sequelize";
+import db from "../config/db";
+
+export class Comment extends Model< InferAttributes<Comment>, InferCreationAttributes<Comment> > {
+    declare id: CreationOptional<number>
+    declare content: string;
+    declare post_id: number;
+    declare user_id: number;
+}
+
+Comment.init({
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    content: {
+        type: DataTypes.TEXT,
+        allowNull: false
+    },
+    post_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: "Posts",
+            key: "id"
+        }
+    },
+    user_id: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: "Users",
+            key: "id"
+        },
+        allowNull: false
+    }
+}, {
+    sequelize: db,
+    modelName: "Comment"
+});
